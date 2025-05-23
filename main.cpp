a036336efd705cf667a0d065f0ef0d50a1f889f9
ます <k024g1021@g.neec.ac.jp>
2025-05-23 15:24:51 +0900

    コミット

--- main.cpp
+++ main.cpp
@@ -1,48 +1,25 @@
-#include <iostream>
+#include <stdio.h>
+#include "IShape.h"
+#include "Circle.h"
+#include "Rectangle.h"
 
-//クラステンプレート
-template <typename Type, typename Type2>
-class TemplateClass {
-public:
-    // コンストラクタ
-    TemplateClass(Type number1, Type2 number2) :
-        Number1(number1), Number2(number2) {
-    }
+int main(void) {
 
-    Type Min()
-    {
-        if (Number1 < Number2)
-            return static_cast<Type>(Number1);
-        else
-            return static_cast<Type>(Number2);
-    }
+	IShape* shape[2];
 
-private:
-    Type Number1;
-    Type2 Number2;
-};
+	shape[0] = new Circle;
+	shape[1] = new Rectangle;
 
-int main() {
+	printf("円の半径:4\n短形の底辺:2\n短形の高さ:3\n");
 
-    TemplateClass<int, float> intFloatTemplate(100, 50.0f);
-    TemplateClass<int, double> intDoubleTemplate(80, 20.0);
-    TemplateClass<float, int> floatIntTemplate(2.0f, 18);
-    TemplateClass<float, double> floatDoubleTemplate(11.0f, 3.5);
-    TemplateClass<double, int> doubleIntTemplate(234.0, 123);
-    TemplateClass<double, float> doubleFloatTemplate(400.1, 500.2f);
+	for (int i = 0; i < 2; i++)
+		shape[i]->Size();
 
-    std::cout << "int(100)　と float(50.0f) を比べて小さい数字を返す：" << intFloatTemplate.Min() << std::endl;
 
-    std::cout << "int(80)　と double(20.0) を比べて小さい数字を返す：" << intDoubleTemplate.Min() << std::endl;
+	printf("\n");
 
-    std::cout << "float(2.0f)　と int(18) を比べて小さい数字を返す：" << floatIntTemplate.Min() << std::endl;
+	for (int i = 0; i < 2; i++)
+		shape[i]->Draw();
 
-    std::cout << "float(11.0f)　と double(3.5) を比べて小さい数字を返す：" << floatDoubleTemplate.Min() << std::endl;
-
-    std::cout << "double(234.0)　と int(123) を比べて小さい数字を返す：" << doubleIntTemplate.Min() << std::endl;
-
-    std::cout << "double(400.1)　と float(500.2) を比べて小さい数字を返す：" << doubleFloatTemplate.Min() << std::endl;
-
-    return 0;
+	return 0;
 }
-
